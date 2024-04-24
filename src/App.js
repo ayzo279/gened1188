6340e52901f454fcbfad28b77d9e88fb5e80636e
Andy Zhuo <azhuo@college.harvard.edu>
2024-04-24 18:49:13 -0400

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar.js';
+import { useEffect, useState } from 'react';
+import SideTab from './components/Sidetab';
+import Posts from './components/Posts';
+import Home from './components/Home';
+import Forum from './components/Forum';
+import Discussion from './components/Discussion';
+
+import { Routes, Route } from "react-router-dom";
+
+
+
 
 function App() {
+
+  useEffect(() => {
+      document.title = "Gened1188"
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Navbar />
+          {/* <SideTab />
+          <Posts /> */}
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/home" element={<Home/>}/>
+              <Route path="/forum" element={<Forum/>}/>
+              <Route path="/discussion/:promptId" element={<Discussion/>} />
+          </Routes>
+          {/* <Projects/> */}
+      </div>
   );
 }
 
